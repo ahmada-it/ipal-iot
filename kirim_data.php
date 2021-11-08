40eb0fa8d3935328a8c3423d084c31e57f91b562
ahmada-it <jamezmada@gmail.com>
2021-11-08 14:31:10 +0700

    add DO sensor

--- kirim_data.php
+++ kirim_data.php
@@ -4,32 +4,40 @@
 	$pH = $_GET['pH'];
     $suhu = $_GET['suhu'];
     $TSS = $_GET['TSS'];
+    $DO = $_GET['DO'];
 	date_default_timezone_set("Asia/Jakarta");
 	$date = date("Y-m-d h:i:s");
 
     if($node == 1){
-        $q = mysqli_query($conn,"INSERT INTO node_ipal_1 (datetime,pH,suhu,TSS) VALUES ('$date','$pH','$suhu','$TSS')");
+        $q = mysqli_query($conn,"INSERT INTO node_ipal_1 (datetime,pH,suhu,TSS,DO) VALUES ('$date','$pH','$suhu','$TSS','$DO')");
         if($q)
         {
             echo "Sukses";
         }
     }
     elseif ($node == 2) {
-        $q = mysqli_query($conn,"INSERT INTO node_ipal_2 (datetime,pH,suhu,TSS) VALUES ('$date','$pH','$suhu','$TSS')");
+        $q = mysqli_query($conn,"INSERT INTO node_ipal_2 (datetime,pH,suhu,TSS,DO) VALUES ('$date','$pH','$suhu','$TSS','$DO')");
         if($q)
         {
             echo "Sukses";
         }
     }
     elseif ($node == 3) {
-        $q = mysqli_query($conn,"INSERT INTO node_ipal_3 (datetime,pH,suhu,TSS) VALUES ('$date','$pH','$suhu','$TSS')");
+        $q = mysqli_query($conn,"INSERT INTO node_ipal_3 (datetime,pH,suhu,TSS,DO) VALUES ('$date','$pH','$suhu','$TSS','$DO')");
         if($q)
         {
             echo "Sukses";
         }
     }
     elseif ($node == 4) {
-        $q = mysqli_query($conn,"INSERT INTO node_ipal_4 (datetime,pH,suhu,TSS) VALUES ('$date','$pH','$suhu','$TSS')");
+        $q = mysqli_query($conn,"INSERT INTO node_ipal_4 (datetime,pH,suhu,TSS,DO) VALUES ('$date','$pH','$suhu','$TSS','$DO')");
+        if($q)
+        {
+            echo "Sukses";
+        }
+    }
+    elseif ($node == 5) {
+        $q = mysqli_query($conn,"INSERT INTO node_ipal_5 (datetime,pH,suhu,TSS,DO) VALUES ('$date','$pH','$suhu','$TSS','$DO')");
         if($q)
         {
             echo "Sukses";
